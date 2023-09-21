ab32b8d8390cc12cc5e01a4442eb75ea05d758a7
Aejkatappaja <joverjlj@gmail.com>
2023-09-21 12:23:31 +0200

    add: editor options

---
+++ src/options/index.ts
@@ -0,0 +1,13 @@
+import { fonts } from "./fonts";
+import { languages } from "./languages";
+import { codeSnippets } from "./snippets";
+import { themes } from "./themes";
+
+const options = {
+  fonts,
+  languages,
+  codeSnippets,
+  themes,
+};
+
+export default options;
