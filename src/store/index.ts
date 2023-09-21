c9da6bb5c534a043a841b4c36ae393c7a98c1fd3
Aejkatappaja <joverjlj@gmail.com>
2023-09-21 23:15:12 +0200

    add: store in zustand

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,63 @@
-import { create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
 interface State {
   code: string;
+  title: string;
+  theme: string;
+  darkMode: boolean;
+  showBackground: boolean;
+  language: string;
+  autoDetectLanguage: boolean;
+  fontSize: number;
+  fontStyle: string;
+  padding: number;
 }
+
+const initialState: State = {
+  code: '',
+  title: 'Untitled',
+  theme: 'hyper',
+  darkMode: true,
+  showBackground: true,
+  language: 'plaintext',
+  autoDetectLanguage: false,
+  fontSize: 18,
+  fontStyle: 'jetBrainsMono',
+  padding: 64,
+};
 
 interface Action {
   setCode: (code: string) => void;
-  remove: () => void;
+  setTitle: (title: string) => void;
+  setTheme: (theme: string) => void;
+  setDarkMode: (darkmode: boolean) => void;
+  setShowBackgound: (backGround: boolean) => void;
+  setLanguage: (language: string) => void;
+  setAutoDetectLanguage: (autoDetectLanguage: boolean) => void;
+  setFontSize: (fontSize: number) => void;
+  setFontStyle: (fontStyle: string) => void;
+  setPadding: (padding: number) => void;
 }
 
 export const useStore = create<State & Action>()(
   devtools(
     persist(
       (set) => ({
-        code: "",
+        ...initialState,
         setCode: (code) => set(() => ({ code })),
-        remove: () => set(() => ({ code: "" })),
+        setTitle: (title) => set(() => ({ title })),
+        setTheme: (theme) => set(() => ({ theme })),
+        setDarkMode: (darkMode) => set(() => ({ darkMode })),
+        setShowBackgound: (showBackground) => set(() => ({ showBackground })),
+        setLanguage: (language) => set(() => ({ language })),
+        setAutoDetectLanguage: (autoDetectLanguage) =>
+          set(() => ({ autoDetectLanguage })),
+        setFontSize: (fontSize) => set(() => ({ fontSize })),
+        setFontStyle: (fontStyle) => set(() => ({ fontStyle })),
+        setPadding: (padding) => set(() => ({ padding })),
       }),
-      { name: "user-settings" }
+      { name: 'user-settings' }
     )
   )
 );
