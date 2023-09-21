7cf8606284df981852588163862fab12389d3687
Aejkatappaja <joverjlj@gmail.com>
2023-09-21 18:53:19 +0200

    add: zustand store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,24 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
+interface State {
+  code: string;
+}
+
+interface Action {
+  setCode: (code: string) => void;
+  remove: () => void;
+}
+
+export const useStore = create<State & Action>()(
+  devtools(
+    persist(
+      (set) => ({
+        code: "",
+        setCode: (code) => set(() => ({ code })),
+        remove: () => set(() => ({ code: "" })),
+      }),
+      { name: "user-settings" }
+    )
+  )
+);
