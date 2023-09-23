4240ed87224c1faa8158a873fdc75831460e1afd
Aejkatappaja <joverjlj@gmail.com>
2023-09-23 12:13:20 +0200

    add: new components and useEffect queryparams

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,30 @@ import CodeEditor from './components/code-editor';
 import { cn } from './lib/utils';
 import options from './options';
 import { useStore } from './store';
-import { Button } from './components/ui/button';
+import { Card, CardContent } from './components/ui/card';
+import ExportOptions from './components/controls/export-options.tsx';
+import ThemeSelect from './components/controls/theme-select/index.tsx';
 
 function App() {
-  const { theme, fontStyle, showBackground, padding, darkMode, setDarkMode } =
-    useStore();
+  const state = useStore();
+  const { theme, fontStyle, showBackground, padding } = state;
   const selectedTheme = options.themes[theme];
   const editorRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    if (queryParams.size === 0) return;
+    const state = Object.fromEntries(queryParams);
+
+    useStore.setState({
+      ...state,
+      code: state.code ? atob(state.code) : '',
+      autoDetectLanguage: state.autoDetectLanguage === 'true',
+      darkMode: state.darkMode === 'true',
+      fontSize: Number(state.fontSize || 18),
+      padding: Number(state.padding || 64),
+    });
+  }, []);
 
   return (
     <main className='dark flex min-h-screen items-center justify-center bg-neutral-950 text-white'>
@@ -25,7 +42,7 @@ function App() {
       />
       <div
         className={cn(
-          'mb-2 overflow-hidden transition-all ease-out',
+          'mb-2 overflow-hidden rounded-xl transition-all ease-out',
           showBackground
             ? options.themes[theme].background
             : 'ring ring-neutral-900'
@@ -33,9 +50,14 @@ function App() {
         style={{ padding }}
         ref={editorRef}
       >
-        <Button onClick={() => setDarkMode(!darkMode)}> Switch Mode </Button>
         <CodeEditor />
       </div>
+      <Card className='fixed bottom-16 mx-6 bg-neutral-900/90 px-8 py-6 backdrop-blur'>
+        <CardContent className='flex flex-wrap gap-6 border-2 p-0'>
+          <ThemeSelect />
+          <ExportOptions targetRef={editorRef} />
+        </CardContent>
+      </Card>
     </main>
   );
 }
