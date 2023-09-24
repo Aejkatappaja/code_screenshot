1dc4d41ceaf4ca221106902a6fd5c91064560d49
Aejkatappaja <joverjlj@gmail.com>
2023-09-24 23:26:09 +0200

    chore: export all ctrl components from 1 file

---
+++ src/components/controls/index.tsx
@@ -0,0 +1,19 @@
+import BackgroundSwitch from './background-switch';
+import ExportOptions from './export-options';
+import FontSelect from './font-select';
+import DarkModeSwitch from './darkmode-switch';
+import FontSizeInput from './font-size-input';
+import PaddingSlider from './padding-slider';
+import ThemeSelect from './theme-select';
+import LanguageSelect from './language-select';
+
+export {
+  BackgroundSwitch,
+  ExportOptions,
+  FontSelect,
+  DarkModeSwitch,
+  FontSizeInput,
+  PaddingSlider,
+  ThemeSelect,
+  LanguageSelect,
+};
