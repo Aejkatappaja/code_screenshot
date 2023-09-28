0bd7bb30b913e443c1fb0d2603c0d4d277ebc7c9
Aejkatappaja <joverjlj@gmail.com>
2023-09-28 18:30:22 +0200

    feat: add footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,16 @@
+import github from '@/assets/icons/git.png';
+
+export default function Footer() {
+  return (
+    <footer className='text-neutral-40 fixed bottom-4 flex  items-center gap-1 text-xs font-medium'>
+      by{' '}
+      <a
+        className=' flex items-center gap-1 text-purple-400/80'
+        href='https://github.https://github.com/Aejkatappaja/Screenshot_Maker/Aejkatappaja'
+        target='_blank'
+      >
+        Frank <img src={github} alt='github-icon' className='h-6 w-6' />
+      </a>{' '}
+    </footer>
+  );
+}
